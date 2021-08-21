1991dee7bb2c4011a59108160a24c758178064b8
sporeball <sporeballdev@gmail.com>
2021-08-20 20:32:23 -0700

    test: split parameters

--- assembler/test.js
+++ assembler/test.js
@@ -15,12 +15,6 @@ global.file = '[code]';
 
 const tentamen = new Tentamen({
   fn: input => assemble(input),
-  before: function (input) {
-    return input.replace(/:/gm, ':/')
-      .split('/')
-      .map(instr => instr.replace(/,/gm, ', '))
-      .join('\n');
-  },
   after: function (output) {
     return decompile(output)
       .slice(0, -1);
@@ -33,20 +27,68 @@ const tentamen = new Tentamen({
     }
   },
   afterError: e => {
-    let message = stripAnsi(e).split('\n')[0];
+    const message = stripAnsi(e).split('\n')[0];
     return message.slice(message.indexOf(':') + 2);
   }
 });
 
+/*
+  base cases
+*/
 tentamen.suite('base cases');
-tentamen.add('simple case', '1,0,0/1,1,0', [[1, 0, 0], [1, 1, 0]]);
-tentamen.add('hex use', '$100,0,0', [[256, 0, 0]]);
-tentamen.add('C replacement', '1,0/1,1', [[1, 0, 6], [1, 1, 12]]);
-tentamen.add('invalid hex use', '$invalid,0', 'invalid hex literal');
 
+tentamen.add(
+  'simple case',
+  `1, 0, 0
+  1, 1, 0`,
+  [[1, 0, 0], [1, 1, 0]]
+);
+tentamen.add(
+  'hex use',
+  '$100, 0, 0',
+  [[256, 0, 0]]
+);
+tentamen.add(
+  'C replacement',
+  `1, 0
+  1, 1`,
+  [[1, 0, 6], [1, 1, 12]]
+);
+
+// errors
+tentamen.add(
+  'invalid hex use',
+  '$invalid, 0',
+  'invalid hex literal'
+);
+
+/*
+  labels
+*/
 tentamen.suite('labels');
-tentamen.add('basic usage', '1,0,0/label:1,1,label', [[1, 0, 0], [1, 1, 6]]);
-tentamen.add('invalid label name', 'Start:1,0,0', 'invalid label name "Start"');
-tentamen.add('duplicate label', 'start:1,0,0/start:1,1,0', 'label "start" already in use');
+
+tentamen.add(
+  'basic usage',
+  `1, 0, 0
+  label:
+  1, 1, label`,
+  [[1, 0, 0], [1, 1, 6]]
+);
+
+// errors
+tentamen.add(
+  'invalid label name',
+  `Start:
+  1, 1, 0`,
+  'invalid label name "Start"'
+);
+tentamen.add(
+  'duplicate label',
+  `start:
+  1, 0, 0
+  start:
+  1, 1, 0`,
+  'label "start" already in use'
+);
 
 tentamen.done();
